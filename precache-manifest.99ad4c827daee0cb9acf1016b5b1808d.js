9fea5be459d650141f66362cc55a691aaf56ee38
PRoman93 <roman_prokopets@mail.ua>
2019-10-06 16:29:47 +0300

    Updates

---
+++ precache-manifest.99ad4c827daee0cb9acf1016b5b1808d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604d421ba2a70706711e7527bc43d607",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "54220cd3767775e3e427",
+    "url": "/my-react-portfolio/static/css/main.95681de4.chunk.css"
+  },
+  {
+    "revision": "2d578fc9c3c2d3b06a7c",
+    "url": "/my-react-portfolio/static/js/2.274818e1.chunk.js"
+  },
+  {
+    "revision": "54220cd3767775e3e427",
+    "url": "/my-react-portfolio/static/js/main.d2f8e63c.chunk.js"
+  },
+  {
+    "revision": "422f674bc1de4c6986c5",
+    "url": "/my-react-portfolio/static/js/runtime-main.4b826312.js"
+  },
+  {
+    "revision": "b2c46023fb15515796c4a0d7c365eba7",
+    "url": "/my-react-portfolio/static/media/images.b2c46023.png"
+  }
+]);
