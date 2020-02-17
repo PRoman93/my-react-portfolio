21d4d78eb54e15e24d6bc9fe95c7b86f448858e1
PRoman93 <roman_prokopets@mail.ua>
2020-02-17 18:47:19 +0200

    plus particle

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ import Projects from "./Projects/Projects";
 import Slogan from "./Slogan/Slogan";
 import Contacts from "./Contacts/Contacts";
 import Footer from "./Footer/Footer";
+import Particles from "react-particles-js";
 
 class App extends Component {
 
     render() {
+
+        const particleOpt = {
+            particles: {
+                number: {
+                    value:150,
+                    density: {
+        enable:true,
+                        value_area:800
+                    }
+                }
+            }
+        }
         return (
             <div className="App">
-                <div className="portfolio">
+                    <Particles classname='particles'
+                    params={particleOpt}/>
                     <Header/>
                     <Main/>
                     <Skills/>
@@ -21,7 +35,6 @@ class App extends Component {
                     <Slogan/>
                     <Contacts/>
                     <Footer/>
-                </div>
             </div>
         );
     }
