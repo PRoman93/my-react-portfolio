aa6bd6c7f94a44aa0c03dd94ff3bed8b51cbb2c0
PRoman93 <roman_prokopets@mail.ua>
2020-02-17 15:01:27 +0200

    another changes with logos

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from "./Footer.module.css";
-import vk from "./vk.png"
-import facebook from "./telegram.png"
-import instagram from "./instagram.jpg"
-import mail from "./mail.ru.jpg"
+import mail from "../assets/mail.svg"
+import vk from "../assets/vk-brands.svg"
+import instagram from "../assets/instagram-brands.svg"
+import telegram from "../assets/telegram-plane-brands.svg"
+import github from "../assets/github_footer.svg"
 
 
 function Footer() {
@@ -12,13 +13,12 @@ function Footer() {
             <div className={styles.container}>
                 <div className={styles.header}>Роман Прокопец</div>
                 <div className={styles.footerContainer}>
-                    <div>
-                        <i class="fab fa-accessible-icon"></i>
-                        {/*<img className={styles.socialIcon} src={vk} alt="" width="50px" height="50px"/>*/}
-                    </div>
-                    <div><img className={styles.socialIcon} src={facebook} width="50px" height="50px" alt=""/></div>
-                    <div><img className={styles.socialIcon} src={mail} width="50px" height="50px"  alt=""/></div>
-                    <div><img className={styles.socialIcon} src={instagram} width="50px" height="50px" alt=""/></div>
+
+                    <div><img className={styles.socialIcon} src={github} alt="GitHub"/></div>
+                    <div><img className={styles.socialIcon} src={vk} alt="VK"/></div>
+                    <div><img className={styles.socialIcon} src={mail} alt="Mail"/></div>
+                    <div><img className={styles.socialIcon} src={telegram} alt="Telegram"/></div>
+                    <div><img className={styles.socialIcon} src={instagram} alt="Instagram"/></div>
                 </div>
                 <div className={styles.copyRights}>© 2020 Все права защищены</div>
             </div>
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './Skills.module.css';
 import HeadContent from "../HeadContent";
-import react from './react-brands.svg'
+import react from '../assets/react-brands.svg'
+import redux from '../assets/redux-logo-9CA6836C12-seeklogo.com.png'
+import css from '../assets/css3-brands.svg'
+import html from '../assets/html5-brands.svg'
+import js from '../assets/js-brands.svg'
+import github from '../assets/github.svg'
 
 function Skills() {
 
@@ -13,38 +18,68 @@ function Skills() {
                     <HeadContent title="Скиллы" />
                     {/*<h2 className={styles.topSkills}>Мои скиллы</h2>*/}
                     {/*<div className={styles.line}></div>*/}
-                    <div>
-                        <span
-                            className={styles.active}>All</span><span>Slideshow</span><span>Video</span><span>Image</span>
-                    </div>
+                    {/*<div>*/}
+                    {/*    <span*/}
+                    {/*        className={styles.active}>All</span><span>Slideshow</span><span>Video</span><span>Image</span>*/}
+                    {/*</div>*/}
                 </div>
+                <div><i className="fa fa-spinner fa-spin"/></div>
                 <div className={styles.otherSkills}>
                     <div className={styles.column3}>
                         <div className={styles.rightSkillsTop}>
-                            <div className={styles.rightSkillsBox_1}></div>
-                            <h3 className={styles.rightSkillsWord}>JS</h3>
+                            <div><img className={styles.skillsImg} src={html} alt=""/></div>
+                            <h3 className={styles.rightSkillsWord}>HTML</h3>
                         </div>
                         <div className={styles.rightSkillsBottom}>
-                            <span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
                         </div>
                     </div>
                     <div className={styles.column3}>
                         <div className={styles.rightSkillsTop}>
-                            <div className={styles.rightSkillsBox_2}></div>
-                            <h3 className={styles.rightSkillsWord}>HTML,CSS</h3>
+                            <div><img className={styles.skillsImg} src={css} alt=""/></div>
+                            <h3 className={styles.rightSkillsWord}>CSS</h3>
                         </div>
                         <div className={styles.rightSkillsBottom}>
-                            <span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
+                        </div>
+                    </div>
+                    <div className={styles.column3}>
+                        <div className={styles.rightSkillsTop}>
+                            <div><img src={js} className={styles.skillsImg} alt=""/></div>
+                            <h3 className={styles.rightSkillsWord}>JS</h3>
+                        </div>
+                        <div className={styles.rightSkillsBottom}>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
                         </div>
                     </div>
                     <div className={styles.column3}>
                         <div className={styles.rightSkillsTop}>
                             <div >
-                                <img src={react} height='100px' width='100px' alt=""/></div>
+                                <img src={react} className={styles.skillsImg} alt=""/></div>
                             <h3 className={styles.rightSkillsWord}>React</h3>
                         </div>
                         <div className={styles.rightSkillsBottom}>
-                            <span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
+                        </div>
+                    </div>
+                    <div className={styles.column3}>
+                        <div className={styles.rightSkillsTop}>
+                            <div >
+                                <img src={redux} className={styles.skillsImg} alt=""/></div>
+                            <h3 className={styles.rightSkillsWord}>Redux</h3>
+                        </div>
+                        <div className={styles.rightSkillsBottom}>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
+                        </div>
+                    </div>
+                    <div className={styles.column3}>
+                        <div className={styles.rightSkillsTop}>
+                            <div >
+                                <img src={github} className={styles.skillsImg} alt=""/></div>
+                            <h3 className={styles.rightSkillsWord}>GitHub</h3>
+                        </div>
+                        <div className={styles.rightSkillsBottom}>
+                            {/*<span className={styles.rightSkillsBottomPhrase}>Подробное описание навыка</span>*/}
                         </div>
                     </div>
                 </div>
